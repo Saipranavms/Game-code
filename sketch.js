f31e604e10e4ced5f03bdf4f8e37990573201c5b
Saipranavms <107045415+Saipranavms@users.noreply.github.com>
2022-06-14 19:38:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,44 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var sea,ship;
+var seaImg,shipImg;
 
+function preload(){
+  //uncomment the code to add animation to ship 
 
-function preload() {
-    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-    trex_collided = loadImage("trex_collided.png");
-    groundImage = loadImage("ground2.png")
+ 
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+ 
+  seaImg = loadImage("sea.png");
 }
 
+function setup(){
+  createCanvas(400,400);
+  background("blue");
 
-function setup() {
-    createCanvas(600, 200);
-//create a trex sprite
-    trex = createSprite(50,160,20,50);
-    trex.addAnimation("running", trex_running);
-    trex.scale = 0.5;
-    invisibleGround=createSprite(200,190,400,10)
-    invisibleGround.visible=false
-//create a ground sprite
-    ground = createSprite(200,180,400,20);
-    ground.addImage("ground",groundImage);
-    ground.x = ground.width /2;
-    ground.velocityX = -4;
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
-
 
 function draw() {
-    background(220);
-//jump when the space button is pressed
-    if (keyDown("space")&&trex.y>=161.5) {
-    trex.velocityY = -10;
-    }
-    trex.velocityY = trex.velocityY + 0.8
-    if (ground.x < 0) {
-    ground.x = ground.width / 2;
-    }
-    console.log(trex.y)
-    trex.collide(invisibleGround);
-    drawSprites();
+  background(0);
+  sea.velocityX = -3;
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    sea.x = 0;
+    sea.x = sea.width;
+    sea.x = sea.width/8;
+    sea.y = height;
+  }
+
+ 
+  drawSprites();
 }
